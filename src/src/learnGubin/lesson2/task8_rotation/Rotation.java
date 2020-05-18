d277751d3c60896f87e5cc5ac4c626088a06dc68
Dmitrii Romanov <dromanovsar@gmail.com>
2020-05-18 23:41:29 +0400

    new solution

--- src/src/learnGubin/lesson2/task8_rotation/Rotation.java
+++ src/src/learnGubin/lesson2/task8_rotation/Rotation.java
@@ -12,12 +12,10 @@ public class Rotation {
             // убираем первый элемент в буфер, а на его место ставим хвостовой элемент
             String buffer = arr[0];
             arr[0] = arr[arr.length - 1];
-
             // циклично сдвигаем весь массив
             for (int j = 1; j < arr.length - 1; j++) {
                 arr[arr.length - j] = arr[arr.length - j - 1];
             }
-
             // ставим буферный элемент в 1 ячейку
             arr[1] = buffer;
             System.out.println(Arrays.toString(arr));
